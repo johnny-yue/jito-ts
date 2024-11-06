7430bc35efa90743c1bec95415bd5f23dc8d3260
John Yue <john.yxn@gmail.com>
2024-11-06 16:43:45 +0800

    poll from multiple block-engine

--- src/examples/simple_bundle/index.ts
+++ src/examples/simple_bundle/index.ts
@@ -1,39 +1,100 @@
 require('dotenv').config();
 
-import * as Fs from 'fs';
+import * as fs from 'fs';
+import * as path from 'path';
 
 import {Keypair, Connection} from '@solana/web3.js';
-
+import * as bip39 from 'bip39';
+import {derivePath} from 'ed25519-hd-key';
 import {searcherClient} from '../../sdk/block-engine/searcher';
 import {onBundleResult, sendBundles} from './utils';
 
+// BIP-44 standard deterministic wallets
+// m     = master seed
+// 44'   = purpose (BIP44)
+// 501'  = coin type (501 is Solana's coin type)
+// 0'    = account index
+// 0'    = change (0 for external, 1 for internal)
+export function createKeypairFromMnemonic(
+  mnemonic: string,
+  accountIndex: number
+) {
+  // Validate a mnemonic
+  const isValid = bip39.validateMnemonic(mnemonic);
+  if (!isValid) {
+    throw new Error('Invalid mnemonic');
+  }
+
+  const path = `m/44'/501'/${accountIndex}'/0'`;
+  const seed = bip39.mnemonicToSeedSync(mnemonic);
+  const derivedSeed = derivePath(path, seed.toString('hex')).key;
+  return Keypair.fromSeed(derivedSeed);
+}
+
 const main = async () => {
-  const blockEngineUrl = process.env.BLOCK_ENGINE_URL || '';
+  const blockEngineUrl = 'mainnet.block-engine.jito.wtf';
   console.log('BLOCK_ENGINE_URL:', blockEngineUrl);
 
-  const authKeypairPath = process.env.AUTH_KEYPAIR_PATH || '';
-  console.log('AUTH_KEYPAIR_PATH:', authKeypairPath);
-  const decodedKey = new Uint8Array(
-    JSON.parse(Fs.readFileSync(authKeypairPath).toString()) as number[]
-  );
-  const keypair = Keypair.fromSecretKey(decodedKey);
+  // const authKeypairPath = '/Users/john/code/solana/keys/J1tozVnYoUrJc9NnjKzbaQEdU9kmTuYKWbdMjRbSpBtC.json';
+  // console.log('AUTH_KEYPAIR_PATH:', authKeypairPath);
+  // const decodedKey = new Uint8Array(
+  //   JSON.parse(Fs.readFileSync(authKeypairPath).toString()) as number[]
+  // );
+  // const keypair = Keypair.fromSecretKey(decodedKey);
 
-  const _accounts = (process.env.ACCOUNTS_OF_INTEREST || '').split(',');
-  console.log('ACCOUNTS_OF_INTEREST:', _accounts);
+  const johnPhantomMnemonic = fs.readFileSync(
+    '/Users/john/code/solana/keys/john.phantom.phrase',
+    'utf8'
+  );
+  const keypair = createKeypairFromMnemonic(johnPhantomMnemonic, 0);
+  console.log(keypair.publicKey.toBase58());
+
+  // const _accounts = (process.env.ACCOUNTS_OF_INTEREST || '').split(',');
+  // console.log('ACCOUNTS_OF_INTEREST:', _accounts);
   // const accounts = _accounts.map(a => new PublicKey(a));
 
-  const bundleTransactionLimit = parseInt(
-    process.env.BUNDLE_TRANSACTION_LIMIT || '0'
-  );
+  const bundleTransactionLimit = 5;
 
-  const c = searcherClient(blockEngineUrl, keypair);
+  const engineURLs = [
+    'mainnet.block-engine.jito.wtf',
+    'amsterdam.mainnet.block-engine.jito.wtf',
+    'frankfurt.mainnet.block-engine.jito.wtf',
+    'ny.mainnet.block-engine.jito.wtf',
+    'tokyo.mainnet.block-engine.jito.wtf',
+    'slc.mainnet.block-engine.jito.wtf',
+    'dallas.testnet.block-engine.jito.wtf',
+    'ny.testnet.block-engine.jito.wtf',
+  ];
 
-  const rpcUrl = process.env.RPC_URL || '';
+  // const client = searcherClient('tokyo.mainnet.block-engine.jito.wtf');
+
+  const clients = engineURLs.map(url => searcherClient(url));
+
+  const rpcUrl =
+    'https://mainnet.helius-rpc.com/?api-key=9a45287f-daed-4752-882d-908d0fbc0d25';
+
   console.log('RPC_URL:', rpcUrl);
   const conn = new Connection(rpcUrl, 'confirmed');
 
-  await sendBundles(c, bundleTransactionLimit, keypair, conn);
-  onBundleResult(c);
+  // const client = await sendBundles(
+  //   clients,
+  //   bundleTransactionLimit,
+  //   keypair,
+  //   conn
+  // );
+
+  const client = await sendBundles(
+    clients,
+    bundleTransactionLimit,
+    keypair,
+    conn
+  );
+
+  console.log('client:', client);
+
+  onBundleResult(client);
+
+  // client.bundleResults
 };
 
 main()
--- src/examples/simple_bundle/utils.ts
+++ src/examples/simple_bundle/utils.ts
@@ -15,12 +15,12 @@ import {isError} from '../../sdk/block-engine/utils';
 const MEMO_PROGRAM_ID = 'Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo';
 
 export const sendBundles = async (
-  c: SearcherClient,
+  clients: SearcherClient[],
   bundleTransactionLimit: number,
   keypair: Keypair,
   conn: Connection
 ) => {
-  const _tipAccount = (await c.getTipAccounts())[0];
+  const _tipAccount = (await clients[0].getTipAccounts())[0];
   console.log('tip account:', _tipAccount);
   const tipAccount = new PublicKey(_tipAccount);
 
@@ -28,13 +28,30 @@ export const sendBundles = async (
   console.log('current account has balance: ', balance);
 
   let isLeaderSlot = false;
+  let client: SearcherClient = clients[0];
+
   while (!isLeaderSlot) {
-    const next_leader = await c.getNextScheduledLeader();
-    const num_slots = next_leader.nextLeaderSlot - next_leader.currentSlot;
-    isLeaderSlot = num_slots <= 2;
-    console.log(`next jito leader slot in ${num_slots} slots`);
+    isLeaderSlot = false;
+    for (const c of clients) {
+      const next_leader = await c.getNextScheduledLeader();
+      const num_slots = next_leader.nextLeaderSlot - next_leader.currentSlot;
+      console.log(`next jito leader slot in ${num_slots} slots`);
+      isLeaderSlot = num_slots <= 2;
+      if (isLeaderSlot) {
+        client = c;
+        break;
+      }
+    }
+    console.log('waiting for leader slot...');
     await new Promise(r => setTimeout(r, 500));
   }
+
+  // while (!isLeaderSlot) {
+  //   const next_leader = await clients.getNextScheduledLeader();
+  //   const num_slots = next_leader.nextLeaderSlot - next_leader.currentSlot;
+  //   isLeaderSlot = num_slots <= 2;
+  //   console.log(`next jito leader slot in ${num_slots} slots`);
+  // }
 
   const blockHash = await conn.getLatestBlockhash();
   const b = new Bundle([], bundleTransactionLimit);
@@ -53,7 +70,7 @@ export const sendBundles = async (
 
   maybeBundle = maybeBundle.addTipTx(
     keypair,
-    100_000,
+    110_000,
     tipAccount,
     blockHash.blockhash
   );
@@ -64,15 +81,19 @@ export const sendBundles = async (
 
   bundles.map(async b => {
     try {
-      const resp = await c.sendBundle(b);
+      const resp = await client.sendBundle(b);
       console.log('resp:', resp);
     } catch (e) {
       console.error('error sending bundle:', e);
     }
   });
+
+  console.log('return from sendBundles');
+  return client;
 };
 
 export const onBundleResult = (c: SearcherClient) => {
+  console.log('onBundleResult');
   c.onBundleResult(
     result => {
       console.log('received bundle result:', result);
@@ -81,6 +102,7 @@ export const onBundleResult = (c: SearcherClient) => {
       throw e;
     }
   );
+  console.log('onBundleResult done');
 };
 
 const buildMemoTransaction = (
